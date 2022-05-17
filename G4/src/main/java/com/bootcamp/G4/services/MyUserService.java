bc4c05f81702d1982d5f9cd19c70dee29450446f
Marcos Hernan Bracciale Sauro <marcoshernanbracciale@gmail.com>
2022-05-17 14:37:18 -0300

    services

---
+++ G4/src/main/java/com/bootcamp/G4/services/MyUserService.java
@@ -0,0 +1,48 @@
+
+package com.bootcamp.G4.services;
+
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserService {
+    
+    @Autowired
+    MyUserRepository uR;
+
+    public ArrayList<MyUser> getAllUsers()) {
+        return (ArrayList<MyUser>) uR.findAll();
+    }
+
+    public MyUser savePerson(MyUser persona) {
+        return uR.save(persona);
+    }
+
+    public MyUser getUserByID(Long id) {
+        return uR.findById(id).get();
+    }
+    //GET BY USER
+    public ArrayList<MyUser> getUserByUserName(String userName) {
+        return uR.findByUserName(userName);
+    }
+    //DELETE
+    public boolean removeUser(Long id) {
+        try {
+            uR.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    //EDIT
+    public boolean editByUserName(MyUser user) {
+        if (uR.findById(user.getId())){
+            uR.save(user);
+            return true;
+        } else
+            return false;        
+    }
+    
+    
+}
---
+++ G4/src/main/java/com/bootcamp/G4/services/WalletService.java
@@ -0,0 +1,44 @@
+
+package com.bootcamp.G4.services;
+
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WalletService {
+    
+    @Autowired
+    WalletRepository wR;
+    
+    @Autowired
+    MyTokenRepository tR;
+
+    public ArrayList<Wallet> getAllWallets()) {
+        return (ArrayList<Wallet>) wR.findAll();
+    }
+
+    public Wallet saveWallet(Wallet wallet) {
+        return wR.save(wallet);
+    }
+
+    public Wallet getWalletByID(Long id) {
+        return wR.findById(id).get();
+    }
+    
+    public boolean removeWallet(Long id) {
+        try {
+            wR.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    /*
+    public TokenWallet addToken(Long idToken, double cantidad)
+    {
+        tokenWallet = 
+        return tokenWallet;
+    }*/
+    
+}
