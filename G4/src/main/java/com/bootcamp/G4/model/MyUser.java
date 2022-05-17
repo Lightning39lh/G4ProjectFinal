48d6028c5e037c6f01ac5a41de05eaf7722800f2
MairaVallejo <vallejomaira218@gmail.com>
2022-05-17 15:12:58 -0500

    Creacion Models

---
+++ G4/src/main/java/com/bootcamp/G4/model/MyUser.java
@@ -0,0 +1,24 @@
+package com.bootcamp.G4.model;
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name= "my_users")
+public class MyUser {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id_user;
+    private String username;  
+    private String password; 
+    private String email;
+
+    @JoinColumn(name = "role_id")
+    private Role role;
+}
