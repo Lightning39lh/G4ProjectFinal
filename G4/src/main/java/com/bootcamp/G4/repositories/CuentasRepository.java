bee61e2d17d53c9f571320d0f9a939e3c3af8dc7
lucaspalomo93 <lucaspalomo.1993@gmail.com>
2022-05-20 10:04:26 -0300

    imports
    Quite imports que no usamos

--- G4/src/main/java/com/bootcamp/G4/repositories/CuentasRepository.java
+++ G4/src/main/java/com/bootcamp/G4/repositories/CuentasRepository.java
@@ -1,7 +1,6 @@
 package com.bootcamp.G4.repositories;
 
 import com.bootcamp.G4.model.Cuentas;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
