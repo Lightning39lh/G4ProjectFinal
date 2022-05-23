20367f5677a5081b8218c1e697db9381823020f1
lucaspalomo93 <lucaspalomo.1993@gmail.com>
2022-05-23 10:29:19 -0300

    borrados imports que no se usan

--- G4/src/main/java/com/bootcamp/G4/model/Ticket.java
+++ G4/src/main/java/com/bootcamp/G4/model/Ticket.java
@@ -1,7 +1,7 @@
 
 package com.bootcamp.G4.model;
 
-import javax.persistence.Column;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
