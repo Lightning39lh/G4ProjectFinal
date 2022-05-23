1a16fd31ce14a7b7f08c2966adf6cd6e50082f4a
Marcos Hernan Bracciale Sauro <marcoshernanbracciale@gmail.com>
2022-05-23 09:41:59 -0300

    MVC

---
+++ mvc/src/main/java/com/bootcamp/mvc/G4mvcApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G4mvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G4mvcApplication.class, args);
+	}
+
+}
---
+++ mvc/src/test/java/com/bootcamp/mvc/G4mvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G4mvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
