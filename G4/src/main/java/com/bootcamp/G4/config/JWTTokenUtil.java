874d315047e73084a8fcd08bd9d318c4265aad78
Claudio Hernandez <93549233+HernandezAriel@users.noreply.github.com>
2022-05-26 17:37:43 -0300

    up

--- G4/src/main/java/com/bootcamp/G4/config/JWTTokenUtil.java
+++ G4/src/main/java/com/bootcamp/G4/config/JWTTokenUtil.java
@@ -1,6 +1,5 @@
 package com.bootcamp.G4.config;
 
-import java.io.Serializable;
 import java.util.Date;
 import java.util.function.Function;
 
@@ -12,10 +11,11 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
-public class JwtTokenUtil implements Serializable {
-    private static final long serialVersionUID = -8091879091924046844L;
+public class JWTTokenUtil {
 
-    private final String SIGNING_KEY = "LaFalopaDelDieguito";
+    //private static final long serialVersionUID = -8091879091924046844L;
+
+    private final String SIGNING_KEY = "privateKey";
 
     private final long ACCESS_TOKEN_VALIDITY_SECONDS = 3600;
 
@@ -30,10 +30,21 @@ public class JwtTokenUtil implements Serializable {
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_VALIDITY_SECONDS*1000))
+                .setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_VALIDITY_SECONDS * 1000))
                 .signWith(SignatureAlgorithm.HS256, SIGNING_KEY)
                 .compact();
-    } 
+    }
+
+
+
+    public Boolean validateToken(String token, UserDetails userDetails){
+
+        final String username = getUsernameFromToken(token);
+
+        return (
+                username.equals(userDetails.getUsername())
+                        && !isTokenExpired(token));
+    }
 
     public String getUsernameFromToken(String token) {
         return getClaimFromToken(token, Claims::getSubject);
@@ -59,11 +70,5 @@ public class JwtTokenUtil implements Serializable {
         final Date expiration = getExpirationDateFromToken(token);
         return expiration.before(new Date());
     }
-
-    public Boolean validateToken(String token, UserDetails userDetails){
-        final String username = getUsernameFromToken(token);
-        return (
-              username.equals(userDetails.getUsername())
-                    && !isTokenExpired(token));
-    }
+    
 }
