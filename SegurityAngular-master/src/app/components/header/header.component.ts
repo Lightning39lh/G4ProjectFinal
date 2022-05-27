c948ff5c22b470b18475fcfded87e6fbc3cf6677
Claudio Hernandez <93549233+HernandezAriel@users.noreply.github.com>
2022-05-27 10:52:47 -0300

    Logout

--- SegurityAngular-master/src/app/components/header/header.component.ts
+++ SegurityAngular-master/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ruta:Router) { }
 
   ngOnInit(): void {
+  }
+
+  logout(){
+    sessionStorage.clear();
+    this.ruta.navigate(['/login']);
   }
 
 }
